500f4f545a4a445b1701db74c0f6011b1c0a1a7b
Immortal813 <rostimmortal@yandex.ru>
2024-05-01 13:26:09 +0300

    ver 0.1377

--- hpp/cock.h
+++ hpp/cock.h
@@ -13,14 +13,14 @@ private:
 public:
 
 
-	std::string voice() override { return "cock"; }
+	std::string voice() override { return "Need more bullets!"; }
 	std::string size() override { return "Smoll"; }
 	std::string weight() override { return "Massive"; }
 	std::string get_name() override { return a_name; }
 
 	std::string get_info() override {
 
-		return "Animal: " + a_name + ", Voice: " + voice() + ", Size: " + size() + ", Weight: " + weight() + "\n";
+		return "Animal name: " + a_name + ", Voice: " + voice() + ", Size: " + size() + ", Weight: " + weight() + "\n";
 
 	}
 
